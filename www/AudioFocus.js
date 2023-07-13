fa6a5db6fa0d517bc9ba9496a86ba6523b73a20a
Afrykakod123 <81309525+Afrykakod123@users.noreply.github.com>
2023-07-13 16:44:17 +0200

    cancel focus

--- www/AudioFocus.js
+++ www/AudioFocus.js
@@ -1,8 +1,14 @@
 var exec = require('cordova/exec');
 
-exports.requestFocus = function(success, error) {
-    success = success || function() { };
-    error = error || function() { };
+exports.requestFocus = function (success, error) {
+  success = success || function () {};
+  error = error || function () {};
 
-    exec(success, error, "AudioFocus", "requestFocus", []);
+  exec(success, error, 'AudioFocus', 'requestFocus', []);
+};
+exports.cancelFocus = function (success, error) {
+  success = success || function () {};
+  error = error || function () {};
+
+  exec(success, error, 'AudioFocus', 'cancelFocus', []);
 };
