caca3d263264d36168624b863b6a7db4d59dcf4e
Afrykakod123 <81309525+Afrykakod123@users.noreply.github.com>
2023-07-13 16:41:43 +0200

    cancel focus

--- src/android/AudioFocus.java
+++ src/android/AudioFocus.java
@@ -17,10 +17,20 @@ public class AudioFocus extends CordovaPlugin {
             this.requestFocus(callbackContext);
             return true;
         }
+          if (action.equals("cancelFocus")) {
+            this.cancelFocus(callbackContext);
+            return true;
+        }
 
         return false;
     }
+    private AudioManager.OnAudioFocusChangeListener listener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
 
+            //nothing to do here
+        }
+    };
     private void requestFocus(CallbackContext callbackContext) {
         // get AudioManager
         AudioManager am = (AudioManager)this.cordova.getActivity()
@@ -28,9 +38,9 @@ public class AudioFocus extends CordovaPlugin {
                                     .getSystemService(Context.AUDIO_SERVICE);
 
         // request audio focus
-        int result = am.requestAudioFocus(null,
+        int result = am.requestAudioFocus(listener,
                                         AudioManager.STREAM_MUSIC,
-                                        AudioManager.AUDIOFOCUS_GAIN);
+                                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 
         // return result
         if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
@@ -39,4 +49,35 @@ public class AudioFocus extends CordovaPlugin {
             callbackContext.error("");
         }
     }
+
+      private void cancelFocus(CallbackContext callbackContext) {
+
+        // get AudioManager
+        AudioManager am = (AudioManager)this.cordova.getActivity()
+                                    .getApplicationContext()
+                                    .getSystemService(Context.AUDIO_SERVICE);
+
+                                    am.abandonAudioFocus(listener);
+
+              // request audio focus
+            //  int result = AudioManager.AUDIOFOCUS_NONE;
+          /*  int result=  am.requestAudioFocus(listener,
+                                              AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_NONE); */
+              // return result
+         /*  int result = am.requestAudioFocus(listener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
+          if (result == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK) {
+            // Fokus dźwięku został przyznany
+            // Wykonaj operacje na dźwięku
+
+            // Anuluj fokus dźwięku
+            am.abandonAudioFocus(listener);
+            callbackContext.success("");
+          }
+          else
+          {
+            am.abandonAudioFocus(listener);
+                    callbackContext.error("");
+           } */
+     }
 }
+
